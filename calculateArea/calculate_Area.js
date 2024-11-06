b32e7e29d108296e56849e0d9c11bc65dcbdd8ab
Baranbuluc <barabuluc@gmail.com>
2024-11-06 14:54:34 -0500

    new version

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -2,9 +2,8 @@ let lenght;
 let width;
 
 function calculateArea() {
-    length = parseFloat(document.getElementById('length').value);
+    lenght = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
-    let area = lenght * width
+    let area = lenght * width;
+    document.getElementById('result').innerText = `The area of the rec is : ${area}`;
 }
-
-document.getElementById('result').innerText = `The area of the rec is : ${area}`;
